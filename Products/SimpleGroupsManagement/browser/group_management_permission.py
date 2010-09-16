c0103d9f3cba497461b606f3c556bd271369a6e6
cekk <andrea.cecchi85@gmail.com>
2010-09-16 15:44:34 +0000

    spostato il tool dalle preferenze degli utenti, al control panel e creato link nelle site_actions

---
+++ Products/SimpleGroupsManagement/browser/group_management_permission.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from Products.Five.browser import BrowserView
+from Products.CMFCore.utils import getToolByName
+from Products.GroupUserFolder.GroupsToolPermissions import ManageGroups
+
+class CheckSimpleGroupsManagement(BrowserView):
+    """View for chek if an user can manage some groups"""
+
+    def __init__(self, context, request):
+        BrowserView.__init__(self, context, request)
+        portal_properties = getToolByName(context, 'portal_properties')
+        self.acl_users = getToolByName(context, 'acl_users')
+        self.sgm_data = portal_properties['simple_groups_management_properties'].sgm_data
+        self.never_used_groups = portal_properties['simple_groups_management_properties'].sgm_never_managed_groups
+        
+    def __call__(self):
+        """Check the simple_groups_management_properties property sheets and find if the user can manage some groups
+        """
+        context = self.context
+        member = getToolByName(context, 'portal_membership').getAuthenticatedMember()
+        if member.has_permission(ManageGroups, context):
+            return True
+
+        my_groups = member.getGroups()
+        for line in self.sgm_data:
+            line = line.strip()
+            if line.find("|")==-1:
+                continue
+            id, group_id = line.split("|")
+            if id==member.getId() or id in my_groups:
+                group = self.acl_users.getGroup(group_id)
+                if group:
+                    return True
+        return False
