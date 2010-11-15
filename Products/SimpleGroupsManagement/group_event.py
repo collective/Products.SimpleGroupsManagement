565ac74bafbb4c4e263d1a27995d60ccd1ccdd36
keul <luca@keul.it>
2010-11-15 15:17:01 +0000

    egg cleaned and fixed versions

--- Products/SimpleGroupsManagement/group_event.py
+++ Products/SimpleGroupsManagement/group_event.py
@@ -2,25 +2,21 @@ from zope.component.interfaces import IObjectEvent,ObjectEvent
 from zope.interface import implements
 
 class IUserAddedToGroup(IObjectEvent):
-    """An user has been added to a group
-    """
+    """Marker interface for user that is added to a group"""
 
 class IUserRemovedFromGroup(IObjectEvent):
-    """An user has been removed from a group
-    """
+    """AMarker interface for user that is removed from a group"""
 
 class UserAddedToGroup(ObjectEvent):
-    """ """
+    """Event fired when a user is added to a group"""
     implements(IUserAddedToGroup)
 
     def __init__(self, group, user_id) :
-        """ """
         super(UserAddedToGroup, self).__init__({'user_id':user_id,'group':group})
 
 class UserRemovedFromGroup(ObjectEvent):
-    """ """
+    """Event fired when a user is removed from a group"""
     implements(IUserRemovedFromGroup)
 
     def __init__(self, group, user_id) :
-        """ """
         super(UserRemovedFromGroup, self).__init__({'user_id':user_id,'group':group})
--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.3.0'
+version = '0.3.0dev'
 
 setup(name='Products.SimpleGroupsManagement',
       version=version,
-      description="A Plone utility to make possible for non-manager users to handle specific groups and users",
+      description="A Plone utility that make possible for non-manager users to manage some groups",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='plone users groups',
+      keywords='plone users groups acl',
       author='keul',
       author_email='sviluppoplone@redturtle.net',
       url='http://svn.plone.org/svn/collective/Products.SimpleGroupsManagement/',
@@ -30,5 +31,4 @@ setup(name='Products.SimpleGroupsManagement',
       entry_points="""
       # -*- Entry points: -*-
       """,
-      paster_plugins = ["ZopeSkel"],
       )
